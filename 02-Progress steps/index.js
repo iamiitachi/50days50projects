4fbb5d0db9ac935ec17213fd703bb8fda74c4664
Sanket <sanketnit0@gmail.com>
2023-04-12 00:48:08 +0530

    revamped progress bar going out of last circle

--- 02-Progress steps/index.js
+++ 02-Progress steps/index.js
@@ -1,42 +1,46 @@
-const progress = document.getElementById('progress');
-const prev = document.getElementById('prev');
-const next = document.getElementById('next');
-const circles = document.querySelectorAll('.circle');
+const circles = document.querySelectorAll(".circle");
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+const progress = document.getElementById("progress");
 
-let currentActive = 1;
+let activeCount = 1;
 
-next.addEventListener('click', () => {
-  currentActive++;
-  if (currentActive > circles.length) {
-    currentActive = circles.length;
+prev.addEventListener("click", () => {
+  activeCount--;
+  if (activeCount < 1) {
+    activeCount = 1;
   }
   update();
 });
 
-prev.addEventListener('click', () => {
-  currentActive--;
-  if (currentActive < 1) {
-    currentActive = 1;
+next.addEventListener("click", () => {
+  activeCount++;
+  if (activeCount > circles.length) {
+    activeCount = circles.length;
   }
   update();
 });
 
 function update() {
-  circles.forEach((circle, index) => {
-    if (index < currentActive) {
-      circle.classList.add('active');
+  circles.forEach((circle, idx) => {
+    if (idx < activeCount) {
+      circle.classList.add("active");
     } else {
-      circle.classList.remove('active');
+      circle.classList.remove("active");
     }
   });
 
-  const actives = document.querySelectorAll('.active');
-  progress.style.width =
-    ((actives.length - 1) / (circles.length - 1)) * 100 + '%';
+  const actives = document.querySelectorAll(".active");
+  console.log(actives.length, circles.length);
 
-  if (currentActive === 1) {
+  progress.style.width =
+    actives.length === circles.length
+      ? ((actives.length - 1) / (circles.length - 1)) * 95 + "%"
+      : ((actives.length - 1) / (circles.length - 1)) * 100 + "%";
+
+  if (activeCount === 1) {
     prev.disabled = true;
-  } else if (currentActive === circles.length) {
+  } else if (activeCount === circles.length) {
     next.disabled = true;
   } else {
     prev.disabled = false;
